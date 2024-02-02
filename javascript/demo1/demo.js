a139e2b12978a8bc151936a3db77e17b3a3168f1
SUSHANT2110 <codesushant2110@gmail.com>
2024-02-02 17:25:17 +0530

    first commit

---
+++ javascript/demo1/demo.js
@@ -0,0 +1,19 @@
+
+console.log("addition is "+(8+7))
+console.log("addition is "+(8+4))
+console.log("addition is "+(8+6))
+console.log("addition is "+(8+2))
+console.log("addition is "+(8+1))
+console.log("addition is "+(8+0))
+console.log("addition is "+(8+8))
+console.log(2>3);
+console.error("hello")
+
+// string concatenation
+
+console.log("hello"+" "+"dear")
+
+// styling the content in console.log
+console.log("%chello this is styling","color:white;border:2px solid black");
+
+// a file saved with .js extension cant directly run on browser so we need to link the js file with html
